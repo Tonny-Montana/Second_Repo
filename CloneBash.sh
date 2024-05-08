8b13830fb6eddd2bea8ee5c98994b73cab846cf8
Tonny-Montana <58221722+Tonny-Montana@users.noreply.github.com>
2024-05-08 21:25:53 +0330

    Update CloneBash.sh
    Second commit for DevOps Project From Cloud

--- CloneBash.sh
+++ CloneBash.sh
@@ -4,3 +4,5 @@ for i in {1..10};do
   echo ${i}
 done
 echo "This is a bash script file form My GitHub"
+read -p "Please Inter your partner's Name : " P_Name
+echo "My Partner's Name is ====> ${P_Name}"
