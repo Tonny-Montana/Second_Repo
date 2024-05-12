1d93fd1adcf65a97384a8646b34949d6a413bec3
Salar <salar.razavi@yahoo.com>
2024-05-12 06:40:49 -0400

    modify ForLoop.sh

--- For_loops.sh
+++ For_loops.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 my_var=10
 
-for i in my_var ;do
-  echo $my_var
-  my_var=$(( my_var-1 ))
-  sleep 0.5
-  if [ my_var le 0 ]
-  exit 0
-done
+while [ $my_var -gt 0 ]
+do
+	echo $my_var
+	my_var=$(( my_var-1 ))
+	sleep 0.5
+done	
